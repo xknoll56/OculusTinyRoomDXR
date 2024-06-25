9692c3cfa47b9a38a7ebd2e640e436e0413652ec
Xavier Knoll <xavierknoll56@gmail.com>
2024-06-24 19:24:57 -0700

    unlit sphere

--- OculusTinyRoomDXR _Spheres/Raytracing.hlsl
+++ OculusTinyRoomDXR _Spheres/Raytracing.hlsl
@@ -82,6 +82,13 @@ struct RayPayload
     uint recursionDepth;
 };
 
+struct Ray
+{
+    float3 origin;
+    float3 direction;
+};
+
+
 // Generate a ray in world space for a camera pixel corresponding to an index from the dispatched 2D grid.
 inline void GenerateCameraRay(uint2 index, out float3 origin, out float3 direction)
 {
@@ -280,6 +287,38 @@ struct ProceduralAttributes
     // Add other attributes as needed
 };
 
+bool IsInRange(in float val, in float min, in float max)
+{
+    return (val >= min && val <= max);
+}
+
+// Test if a hit is culled based on specified RayFlags.
+bool IsCulled(in Ray ray, in float3 hitSurfaceNormal)
+{
+    float rayDirectionNormalDot = dot(ray.direction, hitSurfaceNormal);
+
+    bool isCulled =
+        ((RayFlags() & RAY_FLAG_CULL_BACK_FACING_TRIANGLES) && (rayDirectionNormalDot > 0))
+        ||
+        ((RayFlags() & RAY_FLAG_CULL_FRONT_FACING_TRIANGLES) && (rayDirectionNormalDot < 0));
+
+    return isCulled;
+}
+
+// Test if a hit is valid based on specified RayFlags and <RayTMin, RayTCurrent> range.
+bool IsAValidHit(in Ray ray, in float thit, in float3 hitSurfaceNormal)
+{
+    return IsInRange(thit, RayTMin(), RayTCurrent()) && !IsCulled(ray, hitSurfaceNormal);
+}
+
+void swap(inout float a, inout float b)
+{
+    float temp = a;
+    a = b;
+    b = temp;
+}
+
+
 bool RayAABBIntersectionTest(float3 rayOrigin, float3 rayDir, float3 aabb[2], out float tmin, out float tmax)
 {
     float3 tmin3, tmax3;
@@ -312,8 +351,87 @@ bool RayAABBIntersectionTest(float3 rayOrigin, float3 rayDir, float3 aabb[2], ou
     return tmax > tmin && tmax >= RayTMin() && tmin <= RayTCurrent();
 }
 
-[shader("intersection")]
-void MySimpleIntersectionShader()
+bool SolveQuadraticEqn(float a, float b, float c, out float x0, out float x1)
+{
+    float discr = b * b - 4 * a * c;
+    if (discr < 0)
+        return false;
+    else if (discr == 0)
+        x0 = x1 = -0.5 * b / a;
+    else
+    {
+        float q = (b > 0) ?
+            -0.5 * (b + sqrt(discr)) :
+            -0.5 * (b - sqrt(discr));
+        x0 = q / a;
+        x1 = c / q;
+    }
+    if (x0 > x1)
+        swap(x0, x1);
+
+    return true;
+}
+
+// Calculate a normal for a hit point on a sphere.
+float3 CalculateNormalForARaySphereHit(in Ray ray, in float thit, float3 center)
+{
+    float3 hitPosition = ray.origin + thit * ray.direction;
+    return normalize(hitPosition - center);
+}
+
+// Analytic solution of an unbounded ray sphere intersection points.
+// Ref: https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-sphere-intersection
+bool SolveRaySphereIntersectionEquation(in Ray ray, out float tmin, out float tmax, in float3 center, in float radius)
+{
+    float3 L = ray.origin - center;
+    float a = dot(ray.direction, ray.direction);
+    float b = 2 * dot(ray.direction, L);
+    float c = dot(L, L) - radius * radius;
+    return SolveQuadraticEqn(a, b, c, tmin, tmax);
+}
+
+// Test if a ray with RayFlags and segment <RayTMin(), RayTCurrent()> intersects a hollow sphere.
+bool RaySphereIntersectionTest(in Ray ray, out float thit, out float tmax, out ProceduralAttributes attr, in float3 center = float3(0, 0, 0), in float radius = 1)
+{
+    float t0, t1; // solutions for t if the ray intersects 
+
+    if (!SolveRaySphereIntersectionEquation(ray, t0, t1, center, radius))
+        return false;
+    tmax = t1;
+
+    if (t0 < RayTMin())
+    {
+        // t0 is before RayTMin, let's use t1 instead .
+        if (t1 < RayTMin())
+            return false; // both t0 and t1 are before RayTMin
+
+        attr.normal = CalculateNormalForARaySphereHit(ray, t1, center);
+        if (IsAValidHit(ray, t1, attr.normal))
+        {
+            thit = t1;
+            return true;
+        }
+    }
+    else
+    {
+        attr.normal = CalculateNormalForARaySphereHit(ray, t0, center);
+        if (IsAValidHit(ray, t0, attr.normal))
+        {
+            thit = t0;
+            return true;
+        }
+
+        attr.normal = CalculateNormalForARaySphereHit(ray, t1, center);
+        if (IsAValidHit(ray, t1, attr.normal))
+        {
+            thit = t1;
+            return true;
+        }
+    }
+    return false;
+}
+
+void AABBCollisionTest()
 {
     float tmin, tmax;
     float tHit;
@@ -352,6 +470,21 @@ void MySimpleIntersectionShader()
         else if (distanceToBounds[1].z < eps)
             attr.normal = float3(0, 0, 1);
 
+        //ReportHit(tHit, /*hitKind*/0, attr);
+    }
+}
+
+[shader("intersection")]
+void MySimpleIntersectionShader()
+{
+    float tmin, tmax;
+    float tHit;
+    ProceduralAttributes attr;
+    Ray ray;
+    ray.origin = WorldRayOrigin();
+    ray.direction = WorldRayDirection();
+    if (RaySphereIntersectionTest(ray, tHit, tmax, attr, float3(0.5f, 0.5, 0.5f), 0.5f))
+    {
         ReportHit(tHit, /*hitKind*/0, attr);
     }
 }
@@ -363,7 +496,7 @@ void MyClosestHitShader_AABB(inout RayPayload rayPayload, in ProceduralAttribute
     // PERFORMANCE TIP: it is recommended to minimize values carry over across TraceRay() calls. 
     // Therefore, in cases like retrieving HitWorldPosition(), it is recomputed every time.=
 
-    rayPayload.color = float4(0, 0, 1, 1);
+    rayPayload.color = float4(0, 1, 1, 1);
 }
 
 #endif // RAYTRACING_HLSL
